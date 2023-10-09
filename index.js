4d32e1e8b7f975919a1dee59d9fdbf7829f8689a
Fabio Barros lima <fabi.neto.fb@gmail.com>
2023-10-09 20:28:00 -0300

    Até aqui funciona

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use( bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
@@ -16,21 +18,10 @@ app.set('views', path.join(__dirname,'/pages'));
 
 
 app.get('/',(req,res)=>{
-    console.log(req.query);
 
-    if(req.query.busca == null){
-        res.render('home',{});
-    }else{
-        res.send('Você buscou: '+req.query.busca);
-    }
-
-
-
+   res.send('funcionando')
 
 });
-
-
-
 
 app.get('/:slug',(req,res)=>{
     res.send(req.params.slug);
